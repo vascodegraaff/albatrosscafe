17f22940cf8ee7a3bc9ca25f9fd3825d818563bf
vasco <vascodegraaff@vascos-MacBook-Pro.local>
2019-01-11 17:09:40 +0700

    heroku push

---
+++ build/precache-manifest.51da0f95073877210663196801209940.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "9481048c746aeb33739e58e8388e1c6b",
+    "url": "/static/media/logo.9481048c.png"
+  },
+  {
+    "revision": "f81e4d95a032c198a76d15793b9a98ec",
+    "url": "/static/media/home-info.f81e4d95.jpg"
+  },
+  {
+    "revision": "8970cd70983a2cc388bfd4550c574b16",
+    "url": "/static/media/hero-img.8970cd70.jpg"
+  },
+  {
+    "revision": "722c12d26da195986f3c38bb4035092e",
+    "url": "/static/media/Hero-img-blur.722c12d2.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8f2d0eeac2094a70a875",
+    "url": "/static/js/main.8f2d0eea.chunk.js"
+  },
+  {
+    "revision": "c2782abd3bc1f4c7af89",
+    "url": "/static/js/1.c2782abd.chunk.js"
+  },
+  {
+    "revision": "8f2d0eeac2094a70a875",
+    "url": "/static/css/main.8d840c59.chunk.css"
+  },
+  {
+    "revision": "e50f8f77d6d64e88421b3bb36359e85f",
+    "url": "/index.html"
+  }
+];
